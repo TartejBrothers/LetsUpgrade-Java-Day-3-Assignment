e6c76f29b92f40cee42afd975f56994b3da25ee5
Taranjeet Singh Bedi <taranjeetsinghbedi2@gmail.com>
2023-04-14 11:32:19 +0530

    Add files via upload

---
+++ age.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class age {
+
+    public static void main(String[] args) {
+        Scanner num= new Scanner(System.in);
+        System.out.println("Enter Your Age:");
+        int age_num=num.nextInt();
+        if (age_num>=18){
+            System.out.println("Eligible To Vote"); }
+            else{
+                System.out.println("Not Eligible To Vote");
+}       return;}
+        }
